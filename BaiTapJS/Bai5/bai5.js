0257f6fcd5e944e05539e74d88b9d0bee7d3b787
ngonhattung <ngonhattung08062003@gmail.com>
2024-08-21 13:28:48 +0700

    Buoi1:Bo sung bai 4,hoan thien bai 5

--- BaiTapJS/Bai5/bai5.js
+++ BaiTapJS/Bai5/bai5.js
@@ -1,12 +1,18 @@
-const calcAverage = (value1, value2, value3) => {
-    return (value1 + value2 + value3) / 3
-}
+const calcAverage = (value1, value2, value3) => (value1 + value2 + value3) / 3;
 
 function checkWinner(avgDolhins, avgKoalas) {
-    if (avgDolhins * 2 > avgKoalas) {
-        console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`)
-    } else if (avgKoalas * 2 > avgDolhins)
-    else
-        console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
-
+  if (avgDolhins >= avgKoalas * 2) {
+    console.log(`Dolphins win (${avgDolhins} vs. ${avgKoalas})`);
+  } else if (avgKoalas >= avgDolhins * 2) {
+    console.log(`Koalas win (${avgKoalas} vs. ${avgDolhins})`);
+  } else {
+    console.log(`No team wins`);
+  }
 }
+
+const dataDolphins1 = calcAverage(44, 23, 71);
+const dataKoalas1 = calcAverage(65, 54, 49);
+const dataDolphins2 = calcAverage(85, 54, 41);
+const dataKoalas2 = calcAverage(23, 34, 27);
+checkWinner(dataDolphins1, dataKoalas1);
+checkWinner(dataDolphins2, dataKoalas2);
